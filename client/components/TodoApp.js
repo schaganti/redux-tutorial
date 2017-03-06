dc23ccf4d321610ab8c8ad0ef4845c9525f805e4
schaganti <schaganti@Srinivass-MacBook-Pro.local>
2017-03-05 22:40:19 -0800

    refactored the code

--- client/components/TodoApp.js
+++ client/components/TodoApp.js
@@ -4,6 +4,24 @@ import {store} from '../reducer/todoAppReducer';
 
 let nextId = 1;
 
+const filteredTodos = (todos, filter) => {
+    if (filter === 'SHOW_ALL') {
+        return todos;
+    }
+    if (filter === 'SHOW_ACTIVE') {
+        return todos.filter(todo => {
+            return todo.completed !== true
+        });
+    }
+    if (filter === 'SHOW_COMPLETED') {
+        return todos.filter(todo => {
+            return todo.completed === true
+        });
+    }
+    return store.getState().todos;
+};
+
+
 const FilterLink = React.createClass({
     render: function() {
         const {filter, children, activeFilter} = this.props;
@@ -22,7 +40,8 @@ const FilterLink = React.createClass({
 
 const TodoApp = React.createClass({
     render: function() {
-
+        const {todos, visibilityFilter} = this.props;
+        const filteredTodoList = filteredTodos(todos, visibilityFilter);
         return (
             <div>
                 <input ref={node => {
@@ -38,14 +57,14 @@ const TodoApp = React.createClass({
                 }}>Add Todo</button>
                 <p>
                     <b>Show: </b>
-                    <FilterLink filter="SHOW_ALL" activeFilter={this.props.visibilityFilter}>all</FilterLink>
+                    <FilterLink filter="SHOW_ALL" activeFilter={visibilityFilter}>all</FilterLink>
                     {' '}
-                    <FilterLink filter="SHOW_ACTIVE" activeFilter={this.props.visibilityFilter}>active</FilterLink>
+                    <FilterLink filter="SHOW_ACTIVE" activeFilter={visibilityFilter}>active</FilterLink>
                     {' '}
-                    <FilterLink filter="SHOW_COMPLETED" activeFilter={this.props.visibilityFilter}>completed</FilterLink>
+                    <FilterLink filter="SHOW_COMPLETED" activeFilter={visibilityFilter}>completed</FilterLink>
                 </p>
                 <ul>
-                    {this.props.todos.map(todo => <li key={todo.id} onClick={() => {
+                    {filteredTodoList.map(todo => <li key={todo.id} onClick={() => {
                         store.dispatch({type: 'TOGGLE_TODO', id: todo.id});
                     }} style={{
                         textDecoration: todo.completed
@@ -60,26 +79,10 @@ const TodoApp = React.createClass({
     }
 });
 
-const filteredTodos = () => {
-    if (store.getState().visibilityFilter === 'SHOW_ALL') {
-        return store.getState().todos;
-    }
-    if (store.getState().visibilityFilter === 'SHOW_ACTIVE') {
-        return store.getState().todos.filter(todo => {
-            return todo.completed !== true
-        });
-    }
-    if (store.getState().visibilityFilter === 'SHOW_COMPLETED') {
-        return store.getState().todos.filter(todo => {
-            return todo.completed === true
-        });
-    }
-    return store.getState().todos;
-};
 
 const render = () => {
     ReactDOM.render(
-        <TodoApp todos={filteredTodos()} visibilityFilter={store.getState().visibilityFilter}/>, document.getElementById('root'));
+        <TodoApp {...store.getState()}/>, document.getElementById('root'));
 };
 
 store.subscribe(render);
